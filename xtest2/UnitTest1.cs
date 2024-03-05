95a8e3d6f868aa380f46c473998be62db41c3f72
David Wood <david.wood@saabgroup.com>
2024-03-05 17:10:10 +0000

    Initial commit

---
+++ xtest2/UnitTest1.cs
@@ -0,0 +1,22 @@
+namespace xtest2;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Fail("1");
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        Assert.Fail("2");
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        Assert.Fail("3");
+    }
+}
